4574a646511182c84e5b1b3245747a5f5891d0a9
Renan Augusto <renancandido82@outlook.com>
2024-06-14 12:41:20 -0300

    Speed Insights.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
     className="w-60 shadow-sm position-center"
     />
     <h1 className="font-mono">HELLO, IM RENAN!</h1>
+    <SpeedInsights/>
     </main>
   );
 }
